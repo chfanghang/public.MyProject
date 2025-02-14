23feca8ed8be66d813aa1e4e6e94d46b640d2835
chfanghang <158037729+chfanghang@users.noreply.github.com>
2025-02-14 12:57:24 +0800

    Add files via upload

---
+++ home_work04.java
@@ -0,0 +1,14 @@
+package home_work;
+
+public class home_work04 {
+	public static void main(String[] args) {
+		String name = "李华";
+		int age = 17;
+		double score = 85.6;
+		char sex = '男';
+		String hobby = "打篮球";
+		System.out.print("姓名\t"+"年龄\t"+"成绩\t"+"性别\t"+"爱好\n");
+		System.out.print(name+"\t"+age+"\t"+score+"\t"+sex+"\t"+hobby);
+		
+	}
+}
